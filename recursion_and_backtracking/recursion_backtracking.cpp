5047e8171e25765127882eeff3442610be925e44
goutham120 <nagilagoutham98@gmail.com>
2025-07-10 23:02:34 +0530

    updated recursion-backtracking file!

--- recursion_and_backtracking/recursion_backtracking.cpp
+++ recursion_and_backtracking/recursion_backtracking.cpp
@@ -2,106 +2,100 @@
 #include<string>
 #include<vector>
 #include "consoleUtils.h"
-#include<algorithm>
+#include<stack>
 using namespace std;
-namespace binarysearch{
+namespace recursion{
 struct step{
-     long long low;
-     long long high;
-     long long mid;
-     long long ans;
-     int p;
+    int level;
+    vector<int> curr;
+    vector<vector<int>> ans;
+    string st;
+    string st1;
+    string st2;
+    string st3;
 };
-void reset(vector<int>& nums,int& key){
+void reset(vector<int>& nums){
     gotoxy(0,0);
-    cout<<green<<"----> resetting array and time limit....\n\n";
+    cout<<green<<"----> resetting array....\n\n";
     nums.clear();
     int n;
-    cout<<yellow<<"enter your new array size: "<<white;
+    cout<<yellow<<"enter your new set size: "<<white;
     cin>>n;
     nums.resize(n);
-    cout<<yellow<<"\nenter the array elements:\n"<<white;
+    cout<<green<<"\nenter the set elements:\n"<<white;
     for(int i=0;i<n;i++){
         cin>>nums[i];
     }
-    cout<<yellow<<"\nenter new time limit: "<<white;
-    cin>>key;
 }
-void displaybinary(vector<int> nums,int hours,int low,int high,int mid,int ans,int& p){
-    int Mid=(low+high)/2;
-    cout<<red;
-     cout<<"piles of bananas :[ ";
-    int t=0,tot=0;
-    for(int i=0;i<nums.size();i++){
-        cout<<nums[i]<<" ";
-    }
-    cout<<"]\n\n";
-    cout<<yellow;
-     if(p==0){
-        cout<<"-> lets assume lowest speed is 1 and highest is the max num in the array\n(because if koko can eat highest num of bananas with that speed then remaining bananas can definitely be finished)\n also assume our answer speed is the highest speed\n";  
-     }
-     cout<<red;
-    cout<<"\nanswer range: ["<<low<<","<<high<<"]\n";
-    cout<<cyan<<"lets check its mid as answer : "<<Mid<<"\n";
-    cout<<red<<"\n\nif speed is "<<Mid<<" time taken to eat each pile is: [ ";
-    for(int i=0;i<nums.size();i++){
-          t+=(nums[i]+Mid-1)/Mid;
-          cout<<t<<" ";
-          tot+=t;
-          t=0;
-    }
-    cout<<"] = "<<tot<<"\n\n";
-    if(tot<=hours){
-        ans=Mid;
-        cout<<yellow<<"-> as it took "<<tot<<" hours to eat all bananas which is less/equal to "<<hours<<", "<<Mid<<" is a valid speed\n";
-       if(Mid-1>=low) cout<<purple<<"lets check if lesser speed is possible: that means checking from "<<low<<" to "<<Mid-1<<" (left half)\n\n";
-       else cout<<purple<<" lesser speed is not possible as its already checked\n";
-    }
-    else{ 
-    cout<<yellow<<"-> as it took "<<tot<<" hours to eat all bananas which is greater than "<<hours<<", "<<Mid<<" is not a valid speed\n";
-    if(Mid+1<=high) cout<<purple<<"-> so we have to inrease speed as to eat in lesser time: that means checking from "<<Mid+1<<" to "<<high<<" (right half)\n\n";
-    else cout<<purple<<"-> speed cant be increased furthur as its already checked\n\n";
-    }
-    cout<<green<<"-> valid answer till now: "<<ans<<white;
-}
-bool check(int k,vector<int> nums,int h){
-       int total=0;
-       for(int i:nums){
-          total+=(i+k-1)/k;
+void displaysubsets(vector<int> nums,int ind,vector<int> curr,vector<vector<int>> ans,string st,string st1,string st2,string st3){
+   cout<<red<<"our set : [ ";
+   for(int i=0;i<nums.size();i++){
+    cout<<nums[i]<<" ";
+   }
+   cout<<"]     "<<yellow<<st<<"\n\n\n";
+   cout<<cyan<<"our current set: [ ";
+   for(int i=0;i<curr.size();i++){
+      cout<<curr[i]<<" ";
+   }
+   cout<<"]      "<<yellow<<st1<<"\n\n\n";
+   cout<<green<<"our power set: [ ";
+   for(auto i:ans){
+        cout<<"[ ";
+       for(int j:i){
+        cout<<j<<" ";
        }
-       return total<=h;
+       cout<<"] ";
+   }
+   cout<<"]    "<<yellow<<st3;
 }
-void binarysearch(vector<int> nums,int hours){
-    int p=0;
-    int n=nums.size();
-    vector<step> steps;
-    long long high=*max_element(nums.begin(),nums.end()),low=1;
-    long long ans=high;
-  steps.push_back({low,high,-1,ans,p});
-  p=1;
-    while(low<=high){
-        int mid=(low+high)/2;
-        if(check(mid,nums,hours)){
-            ans=mid;
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-        if(low>high) break;
-           steps.push_back({low,high,mid,ans,p});
-    }
-    int stepptr=0;
+void allsubsets(int i,vector<int> nums,vector<int>& curr,vector<vector<int>>& ans,vector<step>& steps,string st,string st1,string st2,string st3){
+       st="-> we are at level: "+to_string(i);
+       if(i==nums.size()){
+        ans.push_back(curr);
+        st3="-> as we reached the max level,pushed our current set into power set";
+        st1="";
+        steps.push_back({i,curr,ans,st,st1,st2,st3});
+        return;
+       }
+       else{
+         st3="-> still didnt reach level "+to_string(nums.size());
+       }
+       curr.push_back(nums[i]);
+       st1="-> pushed element at index "+to_string(i)+" ,lets move to next level using recursion..";
+       steps.push_back({i,curr,ans,st,st1,st2,st3});
+       allsubsets(i+1,nums,curr,ans,steps,st,st1,st2,st3);
+       
+       st1="-> popped the previous pushed value (back track) and lets move to next level(recursively) without pushing it";
+       curr.pop_back();
+       steps.push_back({i,curr,ans,st,st1,st2,st3});
+       allsubsets(i+1,nums,curr,ans,steps,st,st1,st2,st3);
+
+}
+void subsets(vector<int> nums){
+    string st1,st2,st3,st;
+    st1="-> lets use an array to track subsets";
+    st2="-> we will use recursion to reach higher levels and backtracking to come back to previous level recursively";
+    st3="-> we will push our current subset into our power set if it reaches it max level\nNOTE: max level means set size";
+    
+ int n=nums.size();
+ st="-> as size is "+to_string(n)+" there will be "+to_string(n)+" levels,we are currently at level 0";
+vector<step> steps;
+ vector<vector<int>> ans;
+ vector<int> curr;
+ int i=0;
+ steps.push_back({i,curr,ans,st,st1,st2,st3});
+ allsubsets(i,nums,curr,ans,steps,st,st1,st2,st3);
+     int stepptr=0;
     while(1){
         gotoxy(0,0);
-        cout<<green<<"\n\n-----> BINARY SEARCH ON ANSWER VISUALIZER <------\n\n";
-        cout<<"Q) Given piles of bananas in an array and integer h, find the minimum integer speed such that all bananas can be heaten in h hours\n"<<red<<"LC question - 875";
+        cout<<green<<"\n\n-----> RECURSION-BACKTRACKING VISUALIZER <-----\n\n";
+        cout<<"Q) Find the power set of the given array: "<<red<<"LC question - 78\n\n";
         cout<<cyan;
-        cout<<"\n\npress : 'n' for next step\n'p' for previous step\n'q' to quit\n'r' for resetting the current array\n\n";
-        cout<<red<<"step: "<<stepptr+1<<"/"<<steps.size()<<"\n";
+        cout<<"press : 'n' for next step\n'p' for previous step\n'q' to quit\n'r' for resetting the current array\n\n";
+        cout<<purple<<"step: "<<stepptr+1<<"/"<<steps.size()<<"\n\n";
         step s=steps[stepptr];
-         displaybinary(nums,hours,s.low,s.high,s.mid,s.ans,s.p);
-         char ch=_getch();
+        displaysubsets(nums,s.level,s.curr,s.ans,s.st,s.st1,s.st2,s.st3);
+        char ch=_getch();
         if(ch=='n'||ch=='N'){
            if(stepptr<steps.size()-1) stepptr++;
         }
@@ -109,19 +103,19 @@ void binarysearch(vector<int> nums,int hours){
             if(stepptr>0) stepptr--;
         }
         else if(ch=='r'||ch=='R'){
-            reset(nums,hours);
+            reset(nums);
             gotoxy(0,0);
-            binarysearch(nums,hours);
+            subsets(nums);
         }
         else if(ch=='q'||ch=='Q'){
-            cout<<green<<"\n\n----> prefix sum visualization completed! <----\n\n";
-            cout<<yellow<<"if you want to use different number of bananas and time ,press 'r' or press again 'q' to exit\n";
-          cout<<white;
+            cout<<green<<"\n\n---> backtracking and recursion visualization completed  <----\n\n";
+            cout<<yellow<<"if you want to use different array now ,press 'r' or press again 'q' to exit\n";
+           cout<<white;
            while(1){
             char ch=_getch();
              if(ch=='r'||ch=='R'){
-                reset(nums,hours);
-                binarysearch(nums,hours);
+                reset(nums);
+                subsets(nums);
              }
              else if(ch=='q'||ch=='Q') return;
         }
@@ -131,8 +125,7 @@ void binarysearch(vector<int> nums,int hours){
  }
 }
 void run(){
-    vector<int> nums={30,11,23,4,20};
-    int key=6;
-    binarysearch(nums,key);
-   }
+    vector<int> nums={1,2,4};
+     subsets(nums);
+  }
 }
